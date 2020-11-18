c9053886468ad43756b20f3c3082a224c47e9cb8
GayatriPachkawde <gayatri5kawde@gmail.com>
2020-11-18 16:30:22 +0530

    updated

--- src/components/List/EditArea.js
+++ src/components/List/EditArea.js
@@ -3,11 +3,13 @@ import React from "react";
 function EditArea(props) {
   return (
     <>
-      <textarea
-        className="editTask"
-        onChange={props.change}
-        value={props.value}
-      />
+      <span style={{ display: "block" }}>
+        <textarea
+          className="editTask"
+          onChange={props.change}
+          value={props.value}
+        />
+      </span>
       <button className="saveTask" onClick={props.saveClicked}>
         Save
       </button>
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,18 @@
 import React from "react";
 import App from "../App";
+import "./task.css";
 
 function Task(props) {
   return (
-    <div>
-      <textarea id="task" onChange={props.change} value={props.value} />
+    <div className="heading">
+      <span id="blockDisplay">
+        <textarea
+          id="task"
+          onChange={props.change}
+          value={props.value}
+          placeholder="Enter a task"
+        />
+      </span>
       <button id="btn" onClick={props.buttonClicked}>
         Add
       </button>
